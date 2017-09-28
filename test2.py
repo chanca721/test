e61cb45a27e001eddaf0fdb7473301ffbebd98ef
Kezaram Tharmasegaram <tharmas3@IITS-B473-14.utsc-labs.utoronto.ca>
2017-09-28 09:16:03 -0400

    worked on my branch's test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,8 @@ def main():
     print("Hello world")
     print("Hello world")
     print("Hello world")
-    print("Hello world")
+    for x in range(0,10):
+	print("yo")
 
 if __name__ == "__main__":
     main()
